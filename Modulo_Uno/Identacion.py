48c98bf69f381a303424a1e2bf12fc47d6a32d99
Roman31X <die.haa65@gmail.com>
2023-07-19 16:46:10 -0500

    Dia1

---
+++ Modulo_Uno/Identacion.py
@@ -0,0 +1,7 @@
+#La identacion es esencial en python ya que no maneja 
+#el concepto de la llaves como en java caso contrario
+#no tomara los resultados 
+if( 2 > 3):
+    print("5 es mayor que 3")
+else:
+    print("3 es mayor")
