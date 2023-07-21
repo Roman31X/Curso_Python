e2b3bf34febc71fd79fab6584e98aa3344e98753
Roman31X <die.haa65@gmail.com>
2023-07-21 00:10:17 -0500

    Dia2

--- Modulo_Uno/HolaMundo.py
+++ Modulo_Uno/HolaMundo.py
@@ -1,1 +1,7 @@
-print("Hola mundo")
+#COMENTARIO EN UNA SOLA LINEA
+"""COMENTARIO EN DIVERSAS 
+comentario 1
+comentario 2
+comentario 3
+LINEAS O MULTIPLES"""
+print('Hola mundo')
--- Modulo_Uno/Identacion.py
+++ Modulo_Uno/Identacion.py
@@ -1,7 +1,11 @@
 #La identacion es esencial en python ya que no maneja 
 #el concepto de la llaves como en java caso contrario
 #no tomara los resultados 
-if( 2 > 3):
+if( 5 > 10):
     print("5 es mayor que 3")
 else:
-    print("3 es mayor")
+    print("10 es mayor que 5")
+if(3 < 3):
+    print("3 es mayor que 2")
+else:
+    print("3 es igual que 3")
