da53fd7dfdb2af31d10bd7cf8384f58bcf61a1de
Roman31X <die.haa65@gmail.com>
2023-07-31 19:17:16 -0500

    Dia3

---
+++ Modulo_Uno/10_ControlFlujo.py
@@ -0,0 +1,9 @@
+#Control de flujo de condicionales
+if 5 > 3:
+    print("5 es mayor que 3") #Este bloque se ejecuta si la expresión es verdadera
+
+#Verificar si dos variable son igules
+x = "Hola"
+
+
+
